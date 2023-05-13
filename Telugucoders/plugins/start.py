969723b13f10ab47cf6cece46bdad811a83178c8
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-05-13 18:45:54 +0530

    Update start.py

--- Telugucoders/plugins/start.py
+++ Telugucoders/plugins/start.py
@@ -55,8 +55,8 @@ async def start_(client: Client, message: Message, _):
            InlineKeyboardButton(_["support_btn"], url=f"https://t.me/{GROUP}"), 
            InlineKeyboardButton(_["network_btn"], url=f"https://t.me/{NETWORK}"), 
            ],[
-           InlineKeyboardButton(_["git_repo"], url="https://github.com/Telugucoders/AmalaMusic"), 
-           InlineKeyboardButton(_["owner_btn"], url=f"tg://settings"), 
+           InlineKeyboardButton(_["git_repo"], url="https://t.me/adult_chatting_groupx"), 
+           InlineKeyboardButton(_["owner_btn"], url=f"@dx_manager"), 
            ],[
            InlineKeyboardButton(_["lang_btn"], callback_data="_langs")
            ]]
